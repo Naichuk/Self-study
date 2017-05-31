6dbd5ace562eee1f6518b7499cb243f13ae28318
MaksNS <naichuk.maks@gmail.com>
2017-05-31 13:36:39 +0300

    Add Problem8 and edit Pr7

--- Project_Euler/Problem7.py
+++ Project_Euler/Problem7.py
@@ -1,9 +1,21 @@
+"""By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
+What is the 10 001st prime number?"""
+def isPrime(n):
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+       d += 2
+    return d * d > n
 
-def largest_prime(data, i = 2):
-    while i < data:
-        if data%i != 0:
-            i += 1
-        else:
-            data = data/i
-    return i
+def problem7(data):
+    res = [2]
+    n = 3
+    while True:
+        if isPrime(n):
+            res.append(n)
+        if len(res)==len(range(data)):
+            break
+        n+=2
+    return res[-1]
