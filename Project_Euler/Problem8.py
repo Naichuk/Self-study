b241929ab7688ff00fc9e0a9a7e96da21cab2292
MaksNS <naichuk.maks@gmail.com>
2017-05-31 14:41:31 +0300

    edit problem 8

---
+++ Project_Euler/Problem8.py
@@ -0,0 +1,19 @@
+f=open('data.txt')
+pre_res = 1
+res = 0
+i = 0
+data = []
+result = []
+with open ('data.txt')as f:
+    for line in f:
+        data += line
+data = [int(x) for x in data if x != '\n']
+print (data)
+while len(data)>13:
+    count = 1
+    for item in range(0,13):
+        count *= data[item]
+    result.append(count)
+    data.pop(0)
+
+print(max(result))
